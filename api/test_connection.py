b0f183a34f7328e99075a6aeca5af1d20c31b3fc
Mohit Sridhar <mohitgsridhar@gmail.com>
2024-10-18 18:50:46 -0700

    test stuff

--- api/test_connection.py
+++ api/test_connection.py
@@ -1,51 +1,24 @@
 import pymongo
 import sys
+import json
 
-##Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set and specify the read preference as secondary preferred
-client = pymongo.MongoClient('mongodb://freakathon:mypassword@docdb-2024-10-16-05-06-03.czka0iao0bxo.us-east-2.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&retryWrites=false') 
+## Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set and specify the read preference as secondary preferred
+client = pymongo.MongoClient('mongodb+srv://molit:cr78tq47o4DTEuem@trialize.gs0xa.mongodb.net/?retryWrites=true&w=majority&appName=trialize') 
 
-##Specify the database to be used
-db = client.test
+## Specify the database to be used
+db = client.trialize_main
 
-##Insert a single document
-db.example.insert_one({
-            "name": "Phase 2 Clinical Trial for Type 2 Diabetes Medication",
-            "location": {
-                "city": "San Francisco",
-                "state": "CA",
-                "zip": "94103"
-            },
-            "qualifications": {
-                "ageRange": {
-                    "minAge": 18,
-                    "maxAge": 65
-                },
-                "gender": "Any",
-                "race": "Any",
-                "veteran_status": false,
-                "disability_status": false,
-                "past_conditions": [
-                    "Type 2 Diabetes",
-                    "High Blood Pressure"
-                ],
-                "immunization": "Up-to-date COVID-19 vaccination",
-                "observation": [
-                    "Glucose levels",
-                    "Blood pressure",
-                    "Body mass index"
-                ],
-                "allergies": [
-                    "None"
-                ],
-                "procedure": "Daily oral medication for 12 weeks, with bi-weekly check-ins."
-            }
-        })
+## Load JSON data
+with open('synth_data/trial_data/sample_trials.json') as f:
+    trial_data = json.load(f)
+
+db.trials.insert_many(trial_data['trials'])
 
 ##Find the document that was previously written
-x = db.example.find_one({'name':'Phase 2 Clinical Trial for Type 2 Diabetes Medication'})
+x = db.trials.find_one({'name':"Phase 1 Clinical Trial for Peripheral Artery Disease Treatment"})
 
-##Print the result to the screen
-print(x)
+## Print the result to the screen
+print(x[''])
 
-##Close the connection
+## Close the connection
 client.close()
